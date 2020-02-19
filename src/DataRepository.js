433e1fd5e43ea292e9aac53f62f895f004e028f6
toda93 <dttong93@gmail.com>
2020-02-20 00:21:44 +0700

    sync

--- src/DataRepository.js
+++ src/DataRepository.js
@@ -30,36 +30,21 @@ class DataRepository {
     }
 
 
-
-
-    async createByUser(user_id = null, data = {}, guard = []) {
+    async create(data, guard = [], user_id = null) {
         const Model = this.getModel();
         const item = new Model();
         if (user_id) {
             item.created_id = user_id;
-            item.updated_id = user_id;
+            item.modified_id = user_id;
         }
         return this._save(item, data, guard);
     }
 
-    async create(data, guard = []) {
-        return this.createByUser(null, data, guard);
-    }
-
-    async createWithMetaByUser(user_id = null, data = {}, guard = []) {
-        const Model = this.getModel();
-        const item = new Model();
-        if (user_id) {
-            item.created_id = user_id;
-            item.updated_id = user_id;
-        }
-
-
+    async createWithMeta(data = {}, guard = [], user_id = null) {
         if (data.images && _.isString(data.images)) {
             data.images = JSON.parse(data.images);
             data.thumb = data.images.thumb;
         }
-
         if (data.thumb && _.isString(data.thumb)) {
             data.thumb = JSON.parse(data.thumb);
         }
@@ -67,29 +52,37 @@ class DataRepository {
         data.metadata_keywords = data.metadata_keywords ? data.metadata_keywords : data.title;
         data.metadata_description = data.metadata_description ? data.metadata_description : (data.text_intro ? data.text_intro : data.title);
         data.metadata_image_url = data.metadata_image_url ? data.metadata_image_url : (data.thumb && data.thumb.original ? data.thumb.original : '');
-
-        return this._save(item, data, guard);
-    }
-    
-    async createWithMeta(data = {}, guard = []) {
-        return this.createWithMetaByUser(null, data, guard);
+        return this.create(data, guard, user_id);
     }
 
-    async updateByUser(user_id = null, model_id, data = {}, guard = []) {
+
+    async createByUser(user_id, data = {}, guard = []) {
+        return this.create(data, guard, user_id);
+    }
+
+
+    async createWithMetaByUser(user_id, data = {}, guard = []) {
+        this.createWithMeta(data, guard, user_id);
+    }
+
+
+    async modify(model_id, data, guard = [], user_id = null) {
 
         const item = await this.firstById(model_id);
         if (item) {
             if (user_id) {
-                item.updated_id = user_id;
+                item.modified_id = user_id;
             }
             return this._save(item, data, guard);
         }
         throw new ErrorException(NOT_EXISTS);
     }
 
-    async update(model_id, data, guard = []) {
-        return this.updateByUser(null, model_id, data, guard);
+    async modifyByUser(user_id, model_id, data = {}, guard = []) {
+        return this.modify(model_id, data, guard, user_id);
     }
+
+    
 
     async beforeLoadData(data) {
         return data;
